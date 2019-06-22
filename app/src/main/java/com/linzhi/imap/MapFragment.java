fb6846de118720279f202d87f6dd4d8ce41863b3
xixizhiisme <1056424198@qq.com>
2019-06-22 23:48:16 +0800

    2019.6.22

--- app/src/main/java/com/linzhi/imap/MapFragment.java
+++ app/src/main/java/com/linzhi/imap/MapFragment.java
@@ -1,23 +1,59 @@
 package com.linzhi.imap;
 
 
+import android.content.Context;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
 import com.baidu.mapapi.SDKInitializer;
 
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
+
+import java.util.List;
+
+import static android.content.Context.SENSOR_SERVICE;
+import static android.support.v4.content.ContextCompat.getSystemService;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements BDLocationListener{
     private MapView mapView=null;
+    private BaiduMap mBaiduMap = null;
+    private Context context;
+    private double mLatitude;
+    private double mLongtitude;
+    private BitmapDescriptor mIconLocation;
+    private LocationClient mLocationClient;
+    private boolean isFirstin = true;
+    BitmapDescriptor mCurrentMarker;
+    boolean isFirstLoc = true;
+
 
     public MapFragment() {
         // Required empty public constructor
@@ -25,26 +61,107 @@ public class MapFragment extends Fragment {
 
 
     @Override
+    @NonNull
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         SDKInitializer.initialize(getActivity().getApplicationContext());
         View rootView= inflater.inflate(R.layout.fragment_map, container, false);
         mapView=rootView.findViewById(R.id.mapTextView);
+        mBaiduMap = mapView.getMap();
+// 开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+        //定位初始化
+        mLocationClient = new LocationClient(getActivity().getApplicationContext());
+//通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(10);
+//设置locationClientOption
+        mLocationClient.setLocOption(option);
+//注册LocationListener监听器
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+//开启地图定位图层
+        mLocationClient.start();
         return rootView;
     }
-
+    @Override
     public void onResume(){
         super.onResume();
         mapView.onResume();
     }
+    @Override
     public void onPause(){
         super.onPause();
         mapView.onPause();
     }
-    public void onDestroy(){
-        super.onDestroy();
+    @Override
+    public void onDestroy() {
+        mLocationClient.stop();
+        mBaiduMap.setMyLocationEnabled(false);
         mapView.onDestroy();
+        mapView = null;
+        super.onDestroy();
     }
 
+    @Override
+    public void onReceiveLocation(BDLocation bdLocation) {
+        SensorManager mSensorManager = (SensorManager) getActivity().getSystemService(SENSOR_SERVICE);//获取传感器管理服务
+        MyLocationConfiguration.LocationMode mCurrentMode = MyLocationConfiguration.LocationMode.NORMAL;//地图显示模式:普通
+        mCurrentMarker = null;//null为默认
+        mCurrentMarker = BitmapDescriptorFactory.fromResource(R.drawable.location);//自定义显示定位图标
+    }
+
+    public class MyLocationListener extends BDAbstractLocationListener{
+        @Override
+        public void onReceiveLocation(BDLocation location){
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mapView == null){
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            MyLocationConfiguration config = new
+                    MyLocationConfiguration(
+                    MyLocationConfiguration.LocationMode.NORMAL, true, mIconLocation);
+            mBaiduMap.setMyLocationConfiguration(config);
+            mLatitude = location.getLatitude();
+            mLongtitude = location.getLongitude();
+            //设置起点
+            LatLng mLastLocationData = new LatLng(mLatitude, mLongtitude);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(),
+                        location.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(18.0f);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
+            if (isFirstin) {
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {
+                    // GPS定位结果
+                    Toast.makeText(context, "定位:"+location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                    // 网络定位结果
+                    Toast.makeText(context, "定位:"+location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {
+                    // 离线定位结果
+                    Toast.makeText(context, "定位:"+location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    Toast.makeText(context, "定位:服务器错误", Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    Toast.makeText(context, "定位:网络错误", Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    Toast.makeText(context, "定位:手机模式错误，请检查是否飞行", Toast.LENGTH_SHORT).show();
+                }
+                isFirstin = false;
+            }
+        }
+    }
 }
